3058b037507177a40085ae30ebc970e7de2282bb
NikGav <gnm.spam@gmail.com>
2018-07-01 23:28:41 +0300

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Package for calculate task.
+*
+* @author NikGav (mailto:gnmmail@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+
  public class Calculate {
+	 /**
+     * Main.
+     * @param args - args.
+     */
 	 public static void main(String[] args) {
 		 System.out.println("Hello World");
 	 }
